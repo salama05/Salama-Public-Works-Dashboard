2f9021a4bf0ab327a7daac21940744251a4f92ad
Salama <salama@example.com>
2026-01-19 22:26:06 +0100

    fix(backend): use standard wildcard for Express 5 compatibility

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -20,7 +20,7 @@ app.use('/api', apiRoutes);
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../../frontend/dist')));
 
-    app.get('/:path*', (req, res) =>
+    app.get('*', (req, res) =>
         res.sendFile(path.resolve(__dirname, '../../', 'frontend', 'dist', 'index.html'))
     );
 } else {
