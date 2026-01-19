be43282df1a91ce9f89a4057cf761579444df470
Salama <salama@example.com>
2026-01-19 22:01:00 +0100

    fix(backend): serve frontend static files in production

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import cors from 'cors';
-import dotenv from 'dotenv';
+import path from 'path';
 import mongoose from 'mongoose';
 
 dotenv.config();
@@ -13,11 +13,20 @@ app.use(express.json());
 
 import apiRoutes from './routes/api';
 
-app.get('/', (req, res) => {
-    res.send('API is running...');
-});
-
 app.use('/api', apiRoutes);
+
+// Serve Static Assets in production
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../../frontend/dist')));
+
+    app.get('*', (req, res) =>
+        res.sendFile(path.resolve(__dirname, '../../', 'frontend', 'dist', 'index.html'))
+    );
+} else {
+    app.get('/', (req, res) => {
+        res.send('API is running...');
+    });
+}
 
 
 const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/public-works-dashboard';
